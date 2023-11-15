4901a1228cf9ecf1277963f379640fb983f339b0
Nama Pengguna <bennichaniago2003@gmail.com>
2023-11-16 01:34:58 +0700

    VirtualDemo.java

--- VirtualDemo.java
+++ VirtualDemo.java
@@ -4,14 +4,21 @@ public class VirtualDemo{
         
         Gaji s = new Gaji("Wahyu", "KUBAR", 3, 5000.00);
         Pegawai e = new Gaji("Amin", "Samarinda", 2, 2500.00);
+        Pegawai n = new Pegawai("Budi", "Padang", 1);
         
         System.out.println("Memanggil mailCheck Berdasarkan Referensi Gaji--");
         s.mailCheck();
-        System.out.println(s.info);
+        String info = s.info;
+        System.out.println(info);
 
         System.out.println("\nMemanggil mailCheck Berdasarkan Referensi Pegawai--");
         e.mailCheck();
-        System.out.println(e.info);
+        info = e.info;
+        System.out.println(info);
 
+        System.out.println("\nMemanggil mailCheck Berdasarkan Class Pegawai--");
+        n.mailCheck();
+        info = n.info;
+        System.out.println(info);
     }
 }
