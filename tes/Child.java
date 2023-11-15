3f8f7b93bef62457c3a9aae2da998cd55ab3de20
Benni Putra Chaniago <120644854+Benni2013@users.noreply.github.com>
2023-11-16 01:35:48 +0700

    Delete tes directory

--- tes/Child.java
+++
@@ -1,9 +0,0 @@
-package tes;
-
-public class Child extends Parent {
-    int x = 10;
-    
-    public void tampilInfo(){
-        System.out.println("Ini class Child");
-    }
-}
--- tes/Parent.java
+++
@@ -1,9 +0,0 @@
-package tes;
-
-public class Parent {
-    int x = 5;
-
-    public void tampilInfo(){
-        System.out.println("Ini class Parent");
-    }
-}
